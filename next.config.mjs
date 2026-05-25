8a05755e6da8f89df7ae1bd0025a5aac34fc6ca1
RoniMaity <maityroni1425@gmail.com>
2026-05-25 06:06:31 +0530

    feat: configure development host origins and initialize global Tailwind CSS theme variables

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['scontent-hou1-1.cdninstagram.com'],
   },
+  experimental: {
+    allowedDevOrigins: ["localhost:3000", "10.254.207.208:3000", "192.168.143.208:3000"],
+  },
 };
 
 export default nextConfig;
